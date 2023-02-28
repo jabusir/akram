280dcdc3ed27ac4f4538da5446a002fc18389acf
Jehad Abusir <jabusir@Jehads-MBP.lan>
2023-02-27 17:17:47 -0800

    Abstract away API calls to own file

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,10 @@ import Head from "next/head";
 import HeroSection from "@/components/HeroSection";
 import SecondHomeSection from "@/components/SecondHomeSection";
 import Navbar from "@/components/Navbar";
-import { useEffect } from "react";
+import { getAllProducts } from "@/util/api";
 
 
 export default function Home(props) {
-console.log(props)
-  useEffect(() => {
-  } , [])
 
   return (
     <>
@@ -35,51 +32,6 @@ console.log(props)
 }
 
 export async function getServerSideProps() {
-  // Fetch data from external API
-  // const res = await getAllProducts()
-  // console.log(res)
-  async function shopifyFetch({ query, variables }) {
-    const endpoint = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN;
-    const key = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN;
-    try {
-      const result = await fetch(endpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Shopify-Storefront-Access-Token': key
-        },
-        body: { query, variables } && JSON.stringify({ query, variables })
-      });
-      return {
-        status: result.status,
-        body: await result.json()
-      };
-    } catch (error) {
-      // console.error('Error:', error);
-      return {
-        status: 500,
-        error: 'Error receiving data'
-      };
-    }
-  }
-
-  async function getAllProducts() {
-    return shopifyFetch({
-      query: `{
-          products(sortKey: TITLE, first: 100) {
-            edges{
-              node {
-                id
-                title
-                description
-              }
-            }
-          }
-        }`
-    });
-  }
-
   const products = await getAllProducts()
-  // Pass data to the page via props
   return { props: { products: products.body.data.products.edges.map(({ node }) => node) } }
 }
--- src/util/api.js
+++ src/util/api.js
@@ -1,41 +1,40 @@
 export async function shopifyFetch({ query, variables }) {
-    const endpoint = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN;
-    const key = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN;
-    try {
-      const result = await fetch(endpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Shopify-Storefront-Access-Token': key
-        },
-        body: { query, variables } && JSON.stringify({ query, variables })
-      });
-      console.log(result)
-      return {
-        status: result.status,
-        body: await result.json()
-      };
-    } catch (error) {
-      console.error('Error:', error);
-      return {
-        status: 500,
-        error: 'Error receiving data'
-      };
-    }
+  const endpoint = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN;
+  const key = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN;
+  try {
+    const result = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Shopify-Storefront-Access-Token': key
+      },
+      body: { query, variables } && JSON.stringify({ query, variables })
+    });
+    return {
+      status: result.status,
+      body: await result.json()
+    };
+  } catch (error) {
+    // console.error('Error:', error);
+    return {
+      status: 500,
+      error: 'Error receiving data'
+    };
   }
+}
 
-  export async function getAllProducts() {
-    return shopifyFetch({
-      query: `{
-          products(sortKey: TITLE, first: 100) {
-            edges{
-              node {
-                id
-                title
-                description
-              }
+export async function getAllProducts() {
+  return shopifyFetch({
+    query: `{
+        products(sortKey: TITLE, first: 100) {
+          edges{
+            node {
+              id
+              title
+              description
             }
           }
-        }`
-    });
-  }
+        }
+      }`
+  });
+}

1cea6c2492c9f74e4f275a70c21a99d193d15ad5
Jehad Abusir <jabusir@Jehads-MBP.lan>
2023-03-08 14:51:44 -0800

    Add solar system model

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import GlowingText from "../GlowingText";
 import braceletPic from "public/bracelet.png";
+import Link from "next/link";
 
 export default function HeroSection() {
   return (
     <>
       <div>
-        <GlowingText text={"About"} />
+        <Link href="/about">
+          <GlowingText text={"About"} />
+        </Link>
         <GlowingText text={"Shop"} />
         <GlowingText text={"Contact"} />
       </div>
---
+++ src/pages/about/index.js
@@ -0,0 +1,11 @@
+import SolarSystem from "@/components/SolarSystem";
+
+export default function About() {
+  return (
+    <>
+      <div>
+        <SolarSystem />
+      </div>
+    </>
+  );
+}
